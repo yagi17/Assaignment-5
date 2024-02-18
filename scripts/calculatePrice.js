d9bd76815d4c5e628a2eba5156d79f7f2cd6aea3
yagi17 <nafissadik785@gmail.com>
2024-02-19 02:29:52 +0600

    calculate ticket

--- scripts/calculatePrice.js
+++ scripts/calculatePrice.js
@@ -12,4 +12,37 @@ function updateTotalAmount() {
     }, 0);
 
     totalAmountElement.textContent = totalAmount;
+
+    if (selectedSeats.length === 4) {
+        applyCouponBtn.removeAttribute('disabled');
+    } else {
+        applyCouponBtn.setAttribute('disabled', true);
+    }
+
 };
+
+// apply coupon
+
+function applyCoupon() {
+    const totalAmountElement = document.getElementById('totalamount');
+    const grandTotalElement = document.getElementById('grandtotal');
+    const couponInput = document.getElementById('couponInput');
+    const errorElement = document.getElementById('invalidcoupon');
+
+    let totalAmount = parseInt(totalAmountElement.textContent);
+    let discount = 0;
+
+    // Check if the NEW15 coupon is applied
+    if (couponInput.value.toLowerCase() === 'new15') {
+        discount = 0.15; // 15% discount
+    }
+    // Check if the Couple20 coupon is applied
+    else if (couponInput.value.toLowerCase() === 'couple20') {
+        discount = 0.20; // 20% discount
+    }
+
+    const discountedAmount = totalAmount * discount;
+    const grandTotal = totalAmount - discountedAmount;
+
+    grandTotalElement.innerText = grandTotal.toFixed(2);
+}
